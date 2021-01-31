7ec90255f25f21348ad1b5669eb952ff6fce8a82
Alexa Castner <kieracastner@gmail.com>
2021-01-30 21:28:00 -0800

    restructure WIP

---
+++ lib/pages/base.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+//gradient code
+// decoration: BoxDecoration(
+//               gradient: LinearGradient(
+//                   begin: Alignment.topCenter,
+//                   end: Alignment.bottomCenter,
+//                   colors: [Colors.red, Colors.yellow, Colors.blue])),
