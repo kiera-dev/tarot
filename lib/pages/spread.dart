7b62771e65358729e072584f8a612260e19395ff
Mike Castner <mjcastner@gmail.com>
2022-01-28 23:55:45 -0800

    Tweak card detail aspect ratio

--- lib/pages/spread.dart
+++ lib/pages/spread.dart
@@ -81,6 +81,7 @@ class SpreadPage extends StatelessWidget {
                     heroEnabled: false,
                   ),
                 ),
+                Padding(padding: EdgeInsets.only(bottom: 50)),
               ],
             );
           },
