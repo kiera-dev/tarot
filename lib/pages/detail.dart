a974a75ad20a688cfec93f94a0033a724604ec4b
Mike Castner <mjcastner@gmail.com>
2022-10-05 12:03:40 -0400

    Functional v1 with final assets

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tarot/widgets/common.dart';
 import 'package:tarot/widgets/tarot.dart';
 
@@ -10,7 +11,12 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(this.card.name)),
+      appBar: AppBar(
+        title: Text(
+          this.card.name,
+          style: GoogleFonts.robotoMono(fontSize: 24),
+        ),
+      ),
       body: Container(
         padding: EdgeInsets.all(25),
         decoration: gradientBackground,
@@ -26,7 +32,12 @@ class DetailPage extends StatelessWidget {
             ),
             Expanded(
               flex: 1,
-              child: Text(this.card.description),
+              child: SingleChildScrollView(
+                child: Text(
+                  this.card.description,
+                  style: GoogleFonts.robotoMono(fontSize: 24),
+                ),
+              ),
             ),
           ],
         ),
